f2b572c85c99645bd9c6027edfca38f930118d45
Michael Kingsnorth <michael.kingsnorth@aperimail.com>
2022-06-17 08:02:41 -0700

    Fixed some typo errors in mnk_encrypt.sh comments

--- mnk_encrypt.sh
+++ mnk_encrypt.sh
@@ -15,15 +15,15 @@ SRM=/usr/bin/srm
 # Check to see if the command line arguments are set
 if [ -v 1 ] || [ -v 2 ]
 then
-   # Check to see if we are encrypting and the given file exits
+   # Check to see if we are encrypting and the given file exists
    if [ "$1" == "-e" ] && [ -f "$2" ]
    then
       # Encrypts the file
       ${OPENSSL} enc -aes-256-cbc -md sha512 -pbkdf2 -iter 100000 -salt -in $2 -out $2.dat
-      # safely delete of the original file
+      # Safely deletes the original file
       ${SRM} $2
       echo "${2} encrypted to ${2}.dat"
-   # Check to see if we are decrypting the file and the given file exits   
+   # Check to see if we are decrypting the file and the given file exists   
    elif [ "$1" == "-d" ] && [ -f "${2}.dat" ]
    then
        # Decrypts the file
@@ -35,7 +35,7 @@ then
         echo "Something went wrong!"
     fi
 else
-  # If no argument values are given the following is displated.
+  # If no argument values are given the following is displayed.
   echo "command should be run as \"mnk_encrypt -e or -d filename\""
   echo "-e = encrypt"
   echo "-d = decrypt"
